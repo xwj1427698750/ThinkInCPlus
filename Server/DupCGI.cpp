208bac2490edaed9870026b52b6fc1b14848de51
xinghen1995 <1097382820@qq.com>
2020-09-02 13:23:10 +0000

    Dup函数测试

---
+++ Server/DupCGI.cpp
@@ -0,0 +1,52 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    int ret = bind(sock, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(sock, 5);
+    assert(ret != -1);
+
+    struct sockaddr_in cli_addr;
+    socklen_t cli_addrlen = sizeof(cli_addr);
+    int connfd = accept(sock, (struct sockaddr *)&cli_addr, &cli_addrlen);
+    if (connfd < 0)
+    {
+        printf("errno is %d\n", errno);
+    }
+    else
+    {
+        close(STDOUT_FILENO);
+        dup(connfd);
+        printf("abc\n");
+        close(connfd);
+    }
+    close(sock);
+    return 0;
+}
