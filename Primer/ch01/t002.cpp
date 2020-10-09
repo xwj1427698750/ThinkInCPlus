256ae43d5a6ea3de944b041bebefcfa51bc8ea96
xinghen1995 <1097382820@qq.com>
2020-10-09 23:53:23 +0000

    C++ primer demo

---
+++ Primer/ch01/t002.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter two numbers: " << endl;
+	int v1, v2;
+	cin >> v1 >> v2;
+	cout << v1 << " multi " << v2 << " is " << v1 * v2 << endl;
+
+	return 0;
+}
